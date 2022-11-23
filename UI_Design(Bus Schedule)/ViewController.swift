d19c375d90a42c5d1ac42a67b4eb6c1489db7529
panditRachana <rachupandit93@gmail.com>
2022-11-23 12:02:08 +0530

    Initial Commit

---
+++ UI_Design(Bus Schedule)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI_Design(Bus Schedule)
+//
+//  Created by Rachana Pandit on 23/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
